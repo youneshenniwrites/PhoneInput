dbee0854e27ec765d12a9e9872e1dd42f9280c81
Younes Henni <henni1989younes@gmail.com>
2019-01-03 12:52:51 +0000

    Added basuc phone input with native-base

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, SafeAreaView } from 'react-native'
+// native base imports
+import {
+  Container,
+  Item,
+  Input,
+  Icon
+} from 'native-base'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Container style={styles.infoContainer}>
+          {/* Phone input with native-base */}
+          <Item rounded style={styles.itemStyle}>
+            <Icon
+              active
+              name='call'
+              style={styles.iconStyle}
+            />
+            <Input style={styles.input} />
+          </Item>
+        </Container>
+      </SafeAreaView>
     )
   }
 }
@@ -14,8 +31,28 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#aa73b7',
     justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  infoContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: 200,
+    bottom: 250,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    backgroundColor: '#aa73b7',
+  },
+  iconStyle: {
+    color: '#5a52a5',
+    fontSize: 28,
+    marginLeft: 15
+  },
+  itemStyle: {
+    marginBottom: 10,
   },
 })
